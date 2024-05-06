322be27cbc573979ccbab7ec95e21bda7f08645f
Rensvdk20 <rensvdk@gmail.com>
2024-05-06 13:00:03 +0200

    Added menu dialog

--- LessIsMore/Assets/Scripts/Menu/MenuManager.cs
+++ LessIsMore/Assets/Scripts/Menu/MenuManager.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+public GameObject[] gameObjects;
+    private int currentIndex = 0;
+    private bool allObjectsShown = false;
+
     void Start()
     {
-        
+        // Show the first game object and hide others
+        ShowCurrentGameObject();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Check if all game objects have been shown
+        if (!allObjectsShown)
+        {
+            // Check for mouse click
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Increment index
+                currentIndex++;
+
+                // Show the current game object and hide others
+                ShowCurrentGameObject();
+
+                // Check if all game objects have been shown
+                if (currentIndex >= gameObjects.Length)
+                {
+                    allObjectsShown = true;
+                }
+            }
+        }
+    }
+
+    void ShowCurrentGameObject()
+    {
+        // Hide all game objects
+        foreach (GameObject obj in gameObjects)
+        {
+            obj.SetActive(false);
+        }
+
+        // Show the current game object if it exists
+        if (currentIndex < gameObjects.Length)
+        {
+            gameObjects[currentIndex].SetActive(true);
+        }
     }
 }
